21846f6441eb1bc03e6ac99b36db9a3e3b7d5a1b
HiChon <59905927+chon26909@users.noreply.github.com>
2024-01-03 22:47:38 +0700

    tab screen

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -1,12 +1,55 @@
-import {View, Text} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
+import HomeScreen from '../screens/HomeScreen';
+import CartScreen from '../screens/CartScreen';
+import FavouritesScreen from '../screens/FavouritesScreen';
+import OrderHistoryScreen from '../screens/OrderHistoryScreen';
+import {StyleSheet} from 'react-native';
+import {COLORS} from '../theme/theme';
+import {BlurView} from '@react-native-community/blur';
+
+const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <View>
-      <Text>TabNavigator</Text>
-    </View>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarHideOnKeyboard: true,
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: styles.tabBarStyle,
+        tabBarBackground: () => (
+          <BlurView
+            overlayColor=""
+            blurAmount={15}
+            style={styles.blurViewStyle}
+          />
+        ),
+      }}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Cart" component={CartScreen} />
+      <Tab.Screen name="Favourite" component={FavouritesScreen} />
+      <Tab.Screen name="History" component={OrderHistoryScreen} />
+    </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    height: 80,
+    position: 'absolute',
+    backgroundColor: COLORS.primaryBlackRGBA,
+    borderTopWidth: 0,
+    elevation: 0,
+    borderTopColor: 'transparent',
+  },
+  blurViewStyle: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+});
 
 export default TabNavigator;
